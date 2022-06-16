135741f03f9410dea1c563055a00803db032ebd6
Oleksandr Diudiun <a.diudiun@gmail.com>
2022-06-16 20:47:41 +0200

    examples

--- examples/MDBConnection.php
+++ examples/MDBConnection.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Memcrab\DB\MDB;
-MDB::shutdown();
+
 MDB::declareConnection(
     'write',
     "127.0.0.1",
@@ -15,3 +15,5 @@ MDB::declareConnection(
     "utf8mb4",
     120
 );
+
+MDB::shutdown();
