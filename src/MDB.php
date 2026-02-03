49a7bebc750634141a33ce6f93a0492dd6f3d0e0
Andrey Mokriy <andrey.spawn@gmail.com>
2026-02-03 15:17:13 +0200

    added tryWork and getWork methods to MDB

--- src/MDB.php
+++ src/MDB.php
@@ -321,4 +321,36 @@ class MDB extends \mysqli implements DB
             unset(self::$connections[$key]);
         }
     }
+
+    public function getWork(string $qs)
+    {
+        try {
+            if (!$this->query($qs)) {
+                throw new \Exception("Mysql error: " . $this->error);
+            }
+
+            return $qs;
+        } catch (\Exception $e) {
+            $this->error($e, $qs);
+            throw $e;
+        }
+    }
+
+    public function tryWork(string $qs, string $exception, bool|int $exceptionCode = false)
+    {
+        try {
+            if (!$this->query($qs)) {
+                if ($exceptionCode === false) {
+                    throw new \Exception($exception . $this->error);
+                } else {
+                    throw new \Exception($exception . $this->error, $exceptionCode);
+                }
+            }
+
+            return $qs;
+        } catch (\Exception $e) {
+            $this->error($e, $qs);
+            throw $e;
+        }
+    }
 }
