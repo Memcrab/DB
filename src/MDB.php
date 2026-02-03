9a65aa16b1ad13b9519eb8cb1d6f5ff8e637f27c
Andrey Mokriy <andrey.spawn@gmail.com>
2026-02-03 15:21:33 +0200

    added tryWork method to MDB

--- src/MDB.php
+++ src/MDB.php
@@ -322,20 +322,6 @@ class MDB extends \mysqli implements DB
         }
     }
 
-    public function getWork(string $qs)
-    {
-        try {
-            if (!$this->query($qs)) {
-                throw new \Exception("Mysql error: " . $this->error);
-            }
-
-            return $qs;
-        } catch (\Exception $e) {
-            $this->error($e, $qs);
-            throw $e;
-        }
-    }
-
     public function tryWork(string $qs, string $exception, bool|int $exceptionCode = false)
     {
         try {
